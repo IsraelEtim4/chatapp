3c626dd231fae56a7b764758ffa0a5e6a42f1425
Etim Israel <israeletim4@gmail.com>
2024-06-16 04:40:59 +0100

    Dependencies updated to supported version

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@ import "dotenv/config";
 
 export default {
   "expo": {
-    "name": "my-app",
-    "slug": "my-app",
+    "name": "chatapp",
+    "slug": "chatapp",
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
